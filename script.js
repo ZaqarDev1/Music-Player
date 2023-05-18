6a6d1dd8e86901b24bce9a570515445698055843
ZaqarDev1 <106472873+ZaqarDev1@users.noreply.github.com>
2023-05-18 08:51:10 +0000

    Pending changes exported from your codespace

--- script.js
+++ script.js
@@ -22,29 +22,11 @@ let updateTimer;
 
 const music_list = [
     {
-        img : 'images/stay.png',
-        name : 'Stay',
-        artist : 'The Kid LAROI, Justin Bieber',
-        music : 'music/stay.mp3'
+        img : 'images/OIG.9Ux.jpg',
+        name : 'Go Off',
+        artist : 'AsphaltTEAM',
+        music : 'music/02 Go Off.mp3'
     },
-    {
-        img : 'images/fallingdown.jpg',
-        name : 'Falling Down',
-        artist : 'Wid Cards',
-        music : 'music/fallingdown.mp3'
-    },
-    {
-        img : 'images/faded.png',
-        name : 'Faded',
-        artist : 'Alan Walker',
-        music : 'music/Faded.mp3'
-    },
-    {
-        img : 'images/ratherbe.jpg',
-        name : 'Rather Be',
-        artist : 'Clean Bandit',
-        music : 'music/Rather Be.mp3'
-    }
 ];
 
 loadTrack(track_index);
